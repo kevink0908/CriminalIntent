27bfd32f03ebd55849c919639d667aeb040ceb20
Kevin Kim <kevink000908@gmail.com>
2023-09-28 01:56:55 -0700

    Completed Chapters 9-11, which qualifies for a grade of C.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,15 @@ class CrimeHolder(private val binding: ListItemCrimeBinding)
                     "${crime.title} clicked!",
                     Toast.LENGTH_SHORT
                 ).show()
+            }
+
+            binding.crimeSolved.visibility = if (crime.isSolved)
+            {
+                View.VISIBLE
+            }
+            else
+            {
+                View.GONE
             }
         }
 }
